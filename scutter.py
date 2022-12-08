620762c78847fda162f88b18563201c0f06c1c20
toshi20011210 <japan.toshi.taka@gmail.com>
2022-12-08 15:04:50 +0900

    20221208.1

--- scutter.py
+++ scutter.py
@@ -9,7 +9,9 @@ i=0
 for i in range (0, len(tempArray)):
     splittedTempArray = tempArray[i].split(" ", 1)
     startTime.append(splittedTempArray[0])
-    songName.append(splittedTempArray[1])
+    splittedSplittedTempArray = splittedTempArray[1].split("/", 1)
+    songName.append(splittedSplittedTempArray[0])
+
 
 startTime.append(addString)
 
